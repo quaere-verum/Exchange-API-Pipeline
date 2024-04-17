87baf8e2651f99736e6832ec758d157f92c618e0
quaere-verum <=>
2024-04-17 18:59:37 +0200

    Added config.py to configure predictive model

--- trading_stream/config.py
+++ trading_stream/config.py
@@ -1,6 +1,11 @@
 # This file should implement the TradingModel class which implements a .predict method.
 # See the feature_calculation class for details on how this is used.
 # Include all the relevant import statements, create features and load the relevant models.
+# NOTE: every feature should return a dictionary with keys 'value' and 'position'. The features array
+# will be an array of shape (n, nr_features), and the concurrent.futures.ProcessPoolExecutor is used to
+# calculate features in parallel. Since the result isn't necessarily returned in order,
+# the 'position' key should contain the position (0 <= position < nr_features) so the feature calculator knows
+# where to insert the value.
 import numpy as np
 
 
